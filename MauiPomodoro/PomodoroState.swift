3a7d8af13030646de18ca37fd85a8fb41e286cc0
Justin Gordon <justin.gordon@gmail.com>
2014-06-19 18:13:11 -1000

    click on time

--- MauiPomodoro/PomodoroState.swift
+++ MauiPomodoro/PomodoroState.swift
@@ -9,64 +9,56 @@
 import Foundation
 
 enum Mode: Int {
-    case Work
-    case ShortBreak
-    case LongBreak
+  case Work
+  case ShortBreak
+  case LongBreak
 }
 
-
 class PomodoroState {
-    var startTime: NSDate?
-    var mode: Mode = Mode.Work
-
-    let totalWorkTimeSeconds = 25.0 * 60.0
-    var workTimeSecondsRemaining = 0.0
-    
-    init() {
-        reset()
+  var startTime: NSDate?
+  var mode: Mode = Mode.Work
+  
+  let totalWorkTimeSeconds = 25.0 * 60.0
+  var workTimeSecondsRemaining = 0.0
+  
+  init() {
+    reset()
+  }
+  
+  func reset() {
+    workTimeSecondsRemaining = totalWorkTimeSeconds
+  }
+  
+  func pause() {
+    workTimeSecondsRemaining = secsLeft()
+    startTime = nil
+  }
+  
+  func start() {
+    startTime = NSDate()
+  }
+  
+  func convertSecsToMinSecs(secs: Double) -> String {
+    var mm = Int(secs / 60)
+    var ss = Int(secs % 60)
+    var sss = String(ss)
+    if ss < 10 {
+      sss = "0" + sss
     }
-    
-    func reset() {
-        workTimeSecondsRemaining = totalWorkTimeSeconds
-        startTime = nil
+    return String("\(mm):\(sss)")
+  }
+  
+  func secsLeft() -> Double {
+    if let st = startTime {
+      var elapsed = st.timeIntervalSinceNow
+      var remaining = workTimeSecondsRemaining + elapsed
+      return max(0, remaining)
     }
-
-    func pause() {
-        workTimeSecondsRemaining = secsLeft()
-        startTime = nil
-    }
-    
-    func start() {
-        startTime = NSDate()
-    }
-    
-    func convertSecsToMinSecs(secs: Double) -> String {
-        var mm = Int(secs / 60)
-        var ss = Int(secs % 60)
-        var sss = String(ss)
-        if ss < 10 {
-            sss = "0" + sss
-        }
-        
-        return String("\(mm):\(sss)")
-    }
-
-    func secsLeft() -> Double {
-        if let st = startTime {
-            var elapsed = st.timeIntervalSinceNow
-            var remaining = workTimeSecondsRemaining + elapsed
-            if remaining > 0 {
-                return remaining
-            } else {
-                return 0
-            }
-        } else {
-            return totalWorkTimeSeconds
-        }
-    }
-    
-    func timerValue() -> String {
-        var secs = secsLeft()
-        return convertSecsToMinSecs(secs)
-    }
+    return totalWorkTimeSeconds
+  }
+  
+  func timerValue() -> String {
+    var secs = secsLeft()
+    return convertSecsToMinSecs(secs)
+  }
 }
--- MauiPomodoro/TimerViewController.swift
+++ MauiPomodoro/TimerViewController.swift
@@ -8,59 +8,64 @@
 
 import UIKit
 
+extension Int {
+  var minutes: Int { return self * 60 }
+  var minute: Int { return self * 60 }
+}
+
 class TimerViewController: UIViewController {
-    
-    let workTimeSeconds = 25 * 60
-    
-    //var timer = NSTimer()
-    var timer: NSTimer?
-    var pomodoroState = PomodoroState()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refresh()
+  
+  let workTimeSeconds = 25.minutes
+  var timer: NSTimer?
+  var pomodoroState = PomodoroState()
+  
+  @IBOutlet var timerLabel : UILabel = nil
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    refresh()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func refresh() {
+    self.timerLabel.text = pomodoroState.timerValue()
+  }
+  
+  func startTimer() {
+    pomodoroState.start()
+    timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "refresh", userInfo: nil, repeats: true)
+    UIApplication.sharedApplication().idleTimerDisabled = true
+  }
+  
+  func stopTimer() {
+    if let theTimer = timer {
+      theTimer.invalidate()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    UIApplication.sharedApplication().idleTimerDisabled = false
+    timer = nil
+  }
+  
+  func pauseTimer() {
+    stopTimer()
+    pomodoroState.pause()
+  }
+  
+  @IBAction func resetPressed(sender : UIButton) {
+    pomodoroState.reset()
+    stopTimer()
+    refresh()
+  }
+  
+  @IBAction func startStopButton(sender : AnyObject) {
+    if timer {
+      pauseTimer()
+    } else {
+      startTimer()
     }
-
-    func refresh() {
-        self.timerLabel.text = pomodoroState.timerValue()
-    }
-    
-    @IBOutlet var timerLabel : UILabel = nil
-
-    func startTimer() {
-        pomodoroState.start()
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.5, target: self, selector: "refresh", userInfo: nil, repeats: true)
-    }
-    
-    func stopTimer() {
-        if let theTimer = timer {
-            theTimer.invalidate()
-        }
-        timer = nil
-    }
- 
-    func pauseTimer() {
-        stopTimer()
-        pomodoroState.pause()
-    }
-    
-    @IBAction func resetPressed(sender : UIButton) {
-        pomodoroState.reset()
-        stopTimer()
-        refresh()
-    }
-
-    @IBAction func startStopButton(sender : AnyObject) {
-        if timer {
-            pauseTimer()
-        } else {
-            startTimer()
-        }
-    }
+  }
 }
 
