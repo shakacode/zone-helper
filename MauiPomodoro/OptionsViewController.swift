71906991bdd7b8b2561a729b8e0284d6fdf7cebd
Ben Ward <ben@mauilabs.com>
2015-02-21 10:06:33 -1000

    Use UIDatepicker to change times

--- MauiPomodoro/OptionsViewController.swift
+++ MauiPomodoro/OptionsViewController.swift
@@ -12,9 +12,12 @@ class OptionsViewController: UIViewController {
   
   @IBOutlet var demoModeSwitch: UISwitch?
 
+    @IBOutlet weak var workDatePicker: UIDatePicker!
     @IBOutlet weak var workTimeField: UITextField!
     @IBOutlet weak var shortBreakTimeField: UITextField!
+    @IBOutlet weak var shortBreakDatePicker: UIDatePicker!
     @IBOutlet weak var longBreakTimeField: UITextField!
+    @IBOutlet weak var longBreakDatePicker: UIDatePicker!
    
     
   override func viewDidLoad() {
@@ -31,16 +34,68 @@ class OptionsViewController: UIViewController {
   
   @IBAction func donePressed(sender : UIButton) {
     self.dismissViewControllerAnimated(true, nil)
-    //self.parentViewController.refresh()
     var prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
     prefs.setObject(PomodoroState.convertMinSecsToSecs(workTimeField.text), forKey: "WORKTIME")
     prefs.setObject(PomodoroState.convertMinSecsToSecs(shortBreakTimeField.text), forKey: "SHORTBREAKTIME")
     prefs.setObject(PomodoroState.convertMinSecsToSecs(longBreakTimeField.text), forKey: "LONGBREAKTIME")
     prefs.synchronize()
+    pomodoroState.resetWork()
+    setTimeLabels()
   }
   
-  
-  @IBAction func demoModeToggled(sender: AnyObject)
+    @IBAction func workTimeButtonPressed(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        let date = dateFormatter.dateFromString(self.workTimeField.text)
+        workDatePicker.date = date!
+        shortBreakDatePicker.hidden = true
+        longBreakDatePicker.hidden = true
+        workDatePicker.hidden = false
+    }
+    @IBAction func shortBreakButtonPressed(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        let date = dateFormatter.dateFromString(self.shortBreakTimeField.text)
+        shortBreakDatePicker.date = date!
+        workDatePicker.hidden = true
+        longBreakDatePicker.hidden = true
+        shortBreakDatePicker.hidden = false
+    }
+    @IBAction func longBreakButtonPressed(sender: AnyObject) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        let date = dateFormatter.dateFromString(self.longBreakTimeField.text)
+        longBreakDatePicker.date = date!
+        workDatePicker.hidden = true
+        shortBreakDatePicker.hidden = true
+        longBreakDatePicker.hidden = false
+    }
+    
+    @IBAction func workDatePickerAction(sender: AnyObject) {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        var strDate = dateFormatter.stringFromDate(workDatePicker.date)
+        self.workTimeField.text = strDate
+        workDatePicker.hidden = true
+    }
+    
+    @IBAction func shortBreakDatePickerAction(sender: AnyObject) {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        var strDate = dateFormatter.stringFromDate(shortBreakDatePicker.date)
+        self.shortBreakTimeField.text = strDate
+        shortBreakDatePicker.hidden = true
+    }
+
+    @IBAction func longBreakDatePickerAction(sender: AnyObject) {
+        var dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        var strDate = dateFormatter.stringFromDate(longBreakDatePicker.date)
+        self.longBreakTimeField.text = strDate
+        longBreakDatePicker.hidden = true
+    }
+    
+    @IBAction func demoModeToggled(sender: AnyObject)
   {
     println("Demo toggled to \(demoModeSwitch!.on)")
     demoMode = demoModeSwitch!.on
