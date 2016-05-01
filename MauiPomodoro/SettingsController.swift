d18fc9f7770175c408c38e04f84e881b7d73eb0d
Alexey Karasev <karasev.alexey@gmail.com>
2016-05-01 15:32:31 +0300

    added pickers

--- MauiPomodoro/SettingsController.swift
+++ MauiPomodoro/SettingsController.swift
@@ -10,14 +10,42 @@ import UIKit
 
 class SettingsController: UITableViewController {
 
+    @IBOutlet var timeInputs: [UITextField]!
+    private var dateFormatter: NSDateFormatter!
+    private var firstResponder: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
     }
     
+    
+    @IBAction func editingDidBegin(sender: AnyObject) {
+        let textField = sender as! UITextField
+        
+        let picker = UIDatePicker()
+        picker.datePickerMode = .CountDownTimer
+        if let text = textField.text {
+            let date = dateFormatter.dateFromString(text)!
+            picker.setDate(date, animated: true)
+        }
+        picker.addTarget(self, action: #selector(SettingsController.pickerChanged(_:)), forControlEvents: [.ValueChanged])
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+        textField.inputView = picker
+        firstResponder = textField
     }
-
-
+    
+    
+    @IBAction func editingDidEnd(sender: AnyObject) {
+        let textField = sender as! UITextField
+        textField.inputView = nil
+    }
+    
+    
+    @IBAction func pickerChanged(sender: AnyObject) {
+        let picker = sender as! UIDatePicker
+        firstResponder.text = dateFormatter.stringFromDate(picker.date)
+    }
+    
 }
