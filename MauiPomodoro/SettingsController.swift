5e0b72714007418ee46dabf62622cfd8d222c3cd
Alexey Karasev <karasev.alexey@gmail.com>
2016-05-01 03:19:14 +0300

    initial settings design

---
+++ MauiPomodoro/SettingsController.swift
@@ -0,0 +1,23 @@
+//
+//  SettingsController.swift
+//  ZoneHelper
+//
+//  Created by Алексей Карасев on 01/05/16.
+//  Copyright © 2016 Justin Gordon. All rights reserved.
+//
+
+import UIKit
+
+class SettingsController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
