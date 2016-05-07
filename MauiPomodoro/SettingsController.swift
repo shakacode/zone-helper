ffb107dcd790a885eaec5c8420033c250f54ae87
Alexey Karasev <karasev.alexey@gmail.com>
2016-05-07 12:04:55 +0300

    added contacts to settings

--- MauiPomodoro/SettingsController.swift
+++ MauiPomodoro/SettingsController.swift
@@ -14,6 +14,9 @@ class SettingsController: UITableViewController {
   @IBOutlet weak var demoSwitch: UISwitch!
   private var dateFormatter: NSDateFormatter!
   private var firstResponder: UITextField!
+    
+  
+  @IBOutlet weak var about: UITextView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,6 +30,18 @@ class SettingsController: UITableViewController {
       input.text = times[index]
     }
     demoSwitch.on = Settings.instance.demo
+    
+    let aboutText = NSMutableAttributedString(string: "Designed by the ShakaCode team.\nhttp://www.shakacode.com\n\nPlease contact us for your custom application development needs!")
+    let range = NSRange(location: 0, length: aboutText.length)
+    let color = UIColor(red: 173.0/255, green: 173.0/255, blue: 173.0/255, alpha: 1.0)
+    aboutText.addAttribute(NSForegroundColorAttributeName, value: color, range: range)
+    aboutText.addAttribute(NSFontAttributeName, value: UIFont.systemFontOfSize(14), range: range)
+    let mailRange = (aboutText.string as NSString).rangeOfString("contact us")
+    let mailURL = NSURL(string: "mailto://contact@shakacode.com")!
+    aboutText.addAttribute(NSLinkAttributeName, value: mailURL, range: mailRange)
+    about.attributedText = aboutText
+    about.textContainer.lineFragmentPadding = 0
+    about.textContainerInset = UIEdgeInsetsZero
   }
   
   
